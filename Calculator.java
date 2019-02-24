777b9ad3f713ef157ddfb8c0fb9a884ac28f9f17
RobOller <46613397+RobOller@users.noreply.github.com>
2019-02-23 22:41:39 -0700

    Add files via upload

---
+++ Calculator.java
@@ -0,0 +1,67 @@
+/*
+ * Author Robert Oller
+ * ClassID CSE 360 - Assignment 2
+
+ * Description: To emulate a calculator using basic arithmetic.
+ */
+package cse360assign2;
+
+public class Calculator {
+
+	private int total;
+	
+	/**
+	 * Description: Calculator constructor.
+	 */
+	public Calculator () {
+		total = 0;  // not needed - included for clarity
+	}
+	
+	/**
+	 * Description: To return the total.
+	 * @return 0
+	 */
+	public int getTotal () {
+		return 0;
+	}
+	
+	/**
+	 * Description: To add the given input.
+	 * @param value
+	 */
+	public void add (int value) {
+		
+	}
+	
+	/**
+	 * Description: To subtract the given input.
+	 * @param value
+	 */
+	public void subtract (int value) {
+		
+	}
+	
+	/**
+	 * Description: To multiply the given input.
+	 * @param value
+	 */
+	public void multiply (int value) {
+		
+	}
+	
+	/**
+	 * Description: To divide the given input.
+	 * @param value
+	 */
+	public void divide (int value) {
+		
+	}
+	
+	/**
+	 * Description: To return the history in string form.
+	 * @return String of history.
+	 */
+	public String getHistory () {
+		return "";
+	}
+}
